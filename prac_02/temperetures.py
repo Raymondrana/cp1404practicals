cbe2fd2cc485f998a67974f452b11fd4a49fc3bd
raymondgichohi <raymond.gichohi@.my.jcu.edu.au>
2026-09-22 18:54:02 +1000

    Add main function to temperetures.py

--- prac_02/temperetures.py
+++ prac_02/temperetures.py
@@ -3,23 +3,62 @@ CP1404/CP5632 - Practical
 Program for temperature conversion
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
 
-        fahrenheit = float(input("fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
+# MENU = """C - Convert Celsius to Fahrenheit
+# F - Convert Fahrenheit to Celsius
+# Q - Quit"""
+# print(MENU)
+# choice = input(">>> ").upper()
+# while choice != "Q":
+#     if choice == "C":
+#         celsius = float(input("Celsius: "))
+#         fahrenheit = celsius * 9.0 / 5 + 32
+#         print(f"Result: {fahrenheit:.2f} F")
+#     elif choice == "F":
+#
+#         fahrenheit = float(input("fahrenheit: "))
+#         celsius = 5 / 9 * (fahrenheit - 32)
+#         print(f"Result: {celsius:.2f} C")
+#     else:
+#         print("Invalid option")
+#     print(MENU)
+#     choice = input(">>> ").upper()
+# print("Thank you.")
+
+
+
+
+
+MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+
+def main():
+
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius * 9.0 / 5 + 32
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+
+            fahrenheit = float(input("fahrenheit: "))
+            celsius = 5 / 9 * (fahrenheit - 32)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def do_stuff():
+    """Function docstring"""
+    # statements...
+
+
+
+main()
+
